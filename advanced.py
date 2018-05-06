6d6137195ad989b5561579eb18fd06cb82eb0b1e
Dylan <mulns@outlook.com>
2018-05-06 14:31:59 +0800

    add HistoryCheckpoint class

--- advanced.py
+++ advanced.py
@@ -8,6 +8,46 @@ from keras.callbacks import Callback, TensorBoard
 from keras.engine.topology import Layer
 from keras import backend as K
 
+''' Callbacks '''
+class HistoryCheckpoint(Callback):
+    '''Callback that records events
+        into a `History` object.
+
+        It then saves the history after each epoch into a file.
+        To read the file into a python dict:
+            history = {}
+            with open(filename, "r") as f:
+                history = eval(f.read())
+
+        This may be unsafe since eval() will evaluate any string
+        A safer alternative:
+
+        import ast
+
+        history = {}
+        with open(filename, "r") as f:
+            history = ast.literal_eval(f.read())
+
+    '''
+
+    def __init__(self, filename):
+        super(HistoryCheckpoint, self).__init__()
+        self.filename = filename
+
+    def on_train_begin(self, logs={}):
+        self.epoch = []
+        self.history = {}
+
+    def on_epoch_end(self, epoch, logs={}):
+        self.epoch.append(epoch)
+        for k, v in logs.items():
+            if k not in self.history:
+                self.history[k] = []
+            self.history[k].append(v)
+
+        with open(self.filename, "w") as f:
+            f.write(str(self.history))
+
 '''
 Below is a modification to the TensorBoard callback to perform 
 batchwise writing to the tensorboard, instead of only at the end
